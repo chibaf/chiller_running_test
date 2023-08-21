0749a06ecae80ad829faa2514eee7d60acc2cf80
chibaf <chibaf@mac.com>
2023-08-21 22:25:02 +0900

    Add files via upload

--- chiller_running_temp_test.py
+++ chiller_running_temp_test.py
@@ -13,12 +13,30 @@ ser = serial.Serial(
 
 if len(sys.argv) ==2: # open an output file as name=sys.argv[1]
   f = open(sys.argv[1], "w")
-temps=[0]*10
-x=range(0, 10, 1)
+temps=[0]*100
+settemp=[25]*100
+x=range(0, 100, 1)
+S1_25=b'\x04\x30\x30\x02\x53\x31\x20\x20\x20\x20\x32\x35\x2E\x30\x03\x70'
+S1_35=b'\x04\x30\x30\x02\x53\x31\x20\x20\x20\x20\x33\x35\x2E\x30\x03\x70'
 request_00M1 = b'\x04\x30\x30\x4D\x31\x05'
+flag=0
+itime=0
+temp0=25
 while True:
   try:
     ser.write(request_00M1)
+    if itime==20:
+      if flag==0:
+        temp0=25
+        flag=1
+        ser.write(S1_25)
+      else:
+        flag=0
+        temp0=35
+        ser.write(S1_35)
+      itime=0
+    else:
+      itime=itime+1
     time.sleep(1)
     line = ser.readline()  
     line2 = line.strip().decode("utf-8")
@@ -26,12 +44,15 @@ while True:
     temp=line3[1]
     temp=temp[:-2]   # remove the last two character
     temps.pop(-1)    # remove the last element
+    settemp.pop(-1)
     temps.insert(0,float(temp)) # add element to the first position
+    settemp.insert(0,float(temp0))
     if len(sys.argv) ==2: # write data to file
       f.write(str(temps[0])+"\n")    
-    print(str(temps[0]))      
+    print(str(temp0)+", "+str(temps[0]))      
     plt.clf()
     plt.plot(x,temps)
+    plt.plot(x,settemp)
     plt.pause(0.1)
   except KeyboardInterrupt:
     print ('exiting')
